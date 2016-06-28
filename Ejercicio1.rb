a463d6960a2416283a237bc1792106068383c40c
juanmarin07 <camilopuerta9205@gmail.com>
2016-06-27 20:44:45 -0500

    Ejercicio1 .

---
+++ Ejercicio1.rb
@@ -0,0 +1,29 @@
+class InitialClass
+
+  def self.suma
+
+  puts 2 + 3
+
+  end
+
+  def self.resta
+    puts 5-8
+
+  end
+
+  def self.multiplicacion
+  puts (2*3)
+
+  end
+
+  def self.divi
+    puts 5.0/8
+
+  end
+
+  def self.conca ('valor1','valor2')
+    puts "el valor 1 es: #{valor1} + el valor 2 es: #{valor2}"
+
+  end
+
+end
